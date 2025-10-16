49d361a5dc1ae7f5eddf1b31e9739717a1b17293
leuss1224 <leonardojustojurado@gmail.com>
2025-10-15 19:24:34 -0500

    feat: corrigiendo app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,18 @@ const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
-const errorHandler = require('./middleware/errorHandler');
+const errorHandler = require('./middlewares/errorHandler');
 
 const authRoutes = require('./routes/auth');
 const projectRoutes = require('./routes/projects');
 const userRoutes = require('./routes/users');
+const taskRoutes = require('./routes/tasks');
+const reportRoutes = require('./routes/reports');
+const documentRoutes = require('./routes/documents');
+const activityLogRoutes = require('./routes/activityLogs');
+const notificationRoutes = require('./routes/notifications');
+const chatMessageRoutes = require('./routes/chatMessages');
+
 
 const app = express();
 const path = require('path');
@@ -24,6 +31,12 @@ app.use(rateLimit({ windowMs: 15 * 60 * 1000, max: 200 }));
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
+app.use('/api/', taskRoutes);
+app.use('/api/', reportRoutes);
+app.use('/api/', documentRoutes);
+app.use('/api/', activityLogRoutes);
+app.use('/api/', notificationRoutes);
+app.use('/api/', chatMessageRoutes);
 
 
 // servir la carpeta uploads como pública
